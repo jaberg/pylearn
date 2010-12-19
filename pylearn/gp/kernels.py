3acc963a8b2d0a5dfe883215f87e1c58ee48cc64
james.bergstra <james.bergstra@gmail.com>
2010-12-19 00:43:47 -0500

    fitting via ConvexMixture seems to work

--- pylearn/gp/kernels.py
+++ pylearn/gp/kernels.py
@@ -29,6 +29,9 @@ class SquaredExponentialKernel(object):
         self.__dict__.update(kwargs)
         if self.log_lenscale.ndim!=0:
             raise TypeError('log_lenscale must be scalar', self.log_lenscale)
+    def __str__(self):
+        l = numpy.sqrt(numpy.exp(self.log_lenscale.value) / 2.0)
+        return "SquaredExponentialKernel{l=%s}"%str(l)
 
     @classmethod
     def alloc(cls, l=1):
@@ -61,6 +64,9 @@ class ExponentialKernel(object):
         self.__dict__.update(kwargs)
         if self.log_lenscale.ndim!=0:
             raise TypeError('log_lenscale must be scalar', self.log_lenscale)
+    def __str__(self):
+        l = numpy.exp(self.log_lenscale.value)
+        return "ExponentialKernel{l=%s}"%str(l)
 
     @classmethod
     def alloc(cls, l=1):
@@ -101,50 +107,60 @@ class ConvexMixtureKernel(object):
     Attributes:
     
         kernels -
+        element_ranges - each kernel looks at these elements (default ALL)
         raw_coefs - 
         coefs - 
 
     """
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
-
+    def __str__(self):
+        coefs = self.coefs_f()
+        ks = [str(k) for k in self.kernels]
+        return 'ConvexMixtureKernel{%s}'%(','.join(['%s*%s'%(str(c),s) for c,s in zip(coefs, ks)]))
     @classmethod
-    def alloc(cls, kernels, coefs=None):
+    def alloc(cls, kernels, coefs=None, element_ranges=None):
         if coefs is None:
             raw_coefs = theano.shared(numpy.zeros(len(kernels)))
         else:
             raise NotImplementedError()
+        coefs=TT.nnet.softmax(raw_coefs.dimshuffle('x',0))[0]
+        coefs_f = theano.function([], coefs)
         return cls(
                 kernels=kernels,
-                coefs=TT.nnet.softmax(raw_coefs),
+                coefs=coefs,
+                coefs_f = coefs_f, #DEBUG
                 raw_coefs = raw_coefs,
+                element_ranges=element_ranges,
                 )
 
     def params(self):
         rval = [self.raw_coefs]
-        for k in self.kernels():
+        for k in self.kernels:
             rval.extend(k.params())
         return rval
 
     def K(self, x, y):
         # get the kernel matrix from each sub-kernel
+        if self.element_ranges is None:
+            Ks = [kernel.K(x,y) for kernel in  self.kernels]
+        else:
+            assert len(self.element_ranges) == len(self.kernels)
+            Ks = [kernel.K(x[er[0]:er[1]],y[er[0]:er[1]])
+                    for (kernel,er) in zip(self.kernels, self.element_ranges)]
         # stack them up
+        Kstack = TT.stack(*Ks)
         # multiply by coefs
-        # sum down to one kernel
-        # return
-        pass
-    def K_each_pair(self, x, y):
-        # get the kernel value from each sub-kernel
-        # stack them up
-        # multiply by coefs
-        # sum down to one kernel
-        # return
-        pass
-
+        # and sum down to one kernel
+        K = TT.sum(self.coefs.dimshuffle(0,'x','x') * Kstack,
+                axis=0)
+        return K
 
 #
 # To move into a NEW FILE pylearn/gp/regression.py
 #
+#TODO: make an Op-friendly version of the minimize_nll function
+#      that does not update shared variables by side-effect
 
 class GPR_math(object):
     """
@@ -197,7 +213,7 @@ class GPR_math(object):
         var_x = 1 - (v**2).sum(axis=0)
         return var_x
 
-    def minimize_nll(self):
+    def minimize_nll(self, maxiter=None):
         """
         Fit GPR kernel parameters by minimizing magininal nll.
 
@@ -206,26 +222,47 @@ class GPR_math(object):
         Side effect: chooses optimal kernel parameters.
         """
 
-        nll = theano.function([], self.s_nll())
-        dnll_dparams = theano.function([], TT.grad(self.s_nll(), self.kernel.params()))
+        cost = self.s_nll() + 0.001 * sum([(p**2).sum() for p in self.kernel.params()])
 
+        nll = theano.function([], cost)
+        dnll_dparams = theano.function([], TT.grad(cost, self.kernel.params()))
+        params = self.kernel.params()
         def get_pt():
             #TODO: handle non-scalar parameters...
-            return numpy.asarray([p.value for p in self.kernel.params()])
+            rval = []
+            for p in params:
+                v = p.get_value().flatten()
+                rval.extend(v)
+            return  numpy.asarray(rval)
         def set_pt(pt):
-            params = self.kernel.params()
-            assert len(pt) == len(params)
-            for pt_i, p_i in zip(pt, params):
-                p_i.value = pt_i
+            i = 0
+            for p in params:
+                shape = p.get_value(borrow=True).shape
+                size = numpy.prod(shape)
+                p.set_value(pt[i:i+size].reshape(shape))
+                i += size
+            assert i == len(pt)
+            print self.kernel
+            print cost.dtype
         def f(pt):
+            print 'f', pt
             set_pt(pt)
             return nll()
         def df(pt):
+            print 'df', pt
             set_pt(pt)
             dparams = dnll_dparams()
-            return numpy.asarray(dparams)
+            rval = []
+            for dp in dparams:
+                rval.extend(dp.flatten())
+            rval =  numpy.asarray(rval)
+            print numpy.sqrt((rval**2).sum())
+            return rval
         start_pt = get_pt()
-        best_pt = scipy.optimize.fmin_ncg(f, start_pt, df)
+        # WEIRD: I was using fmin_ncg here
+        #        until I used a low multiplier on the sum-squared-error regularizer
+        #        on the 'cost' above, which threw ncg into an inf loop!?
+        best_pt = scipy.optimize.fmin_cg(f, start_pt, df, maxiter=maxiter)
         set_pt(best_pt)
 
     def mean(self, x):
--- pylearn/gp/theano_hack2.py
+++ pylearn/gp/theano_hack2.py
@@ -5,22 +5,33 @@ import kernels
 N_train_pts = 8
 
 rng = numpy.random.RandomState(2346)
-x = (numpy.arange(N_train_pts) * 1.0).reshape(N_train_pts,1)
-y = rng.randn(N_train_pts) +x[:,0]
+x0 = (numpy.arange(N_train_pts) * 1.0).reshape(N_train_pts,1)
+y0 = rng.randn(N_train_pts) +x0[:,0]
+x1 = (numpy.arange(N_train_pts) * 1.0).reshape(N_train_pts,1)+0.5
+y1 = rng.randn(N_train_pts) +x0[:,0]
+
+x = numpy.vstack([x0,x1])
+y = numpy.hstack([y0,y1])
 y = y - y.mean() 
 
 x_new = (numpy.arange(0,N_train_pts,0.1)).reshape(N_train_pts/0.1,1)
 
-if 1:
+if 0:
     kernel = kernels.ConvexMixtureKernel.alloc([
         kernels.SquaredExponentialKernel.alloc(),
         kernels.ExponentialKernel.alloc()
+            ])
+elif 1:
+    kernel = kernels.ConvexMixtureKernel.alloc(
+            kernels = [
+                kernels.SquaredExponentialKernel.alloc(),
+                kernels.ExponentialKernel.alloc()
             ])
 else:
     kernel = kernels.ExponentialKernel.alloc()
 # fit the kernels
 gpr = kernels.GPR_math(kernel, x, y, var_y=0.2)
-gpr.minimize_nll()
+gpr.minimize_nll(maxiter=50)
 meanstar, Vfstar = gpr.mean_variance(x_new)
 # get the predictions at new points
 stddev = numpy.sqrt(Vfstar)
